d6b0e61b0d730ea8cfeeef0a613ab009c2a0aa09
tommy <500@superscafe.com>
2021-07-16 12:40:33 -0400

    add – css(2)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,9 @@ import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 
+import "normalize.css"
+import "../assets/css/main.css"
+
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
