1d83e5d4a84b1f89b06f287e5dbaec5fb573cd27
tommy <500@superscafe.com>
2021-07-29 12:15:52 -0400

    remove – /src_hold

--- src_hold/other/Button.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-export const StyledButton = styled.button`
-  padding: 0.05em 0.2em 0.15em 0.2em;
-  margin: -0.3em 0.3em 0.3em 0.2em;
-  background: pink;
-  color: violet;
-  font-size: 2.5rem;
-`
