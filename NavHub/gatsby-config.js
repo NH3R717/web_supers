9a8c59004e2797a49697a566d485716cb68e31be
tommy <500@superscafe.com>
2021-08-02 12:14:06 -0400

    add – webfonts plugin

--- NavHub/gatsby-config.js
+++ NavHub/gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
   /* Your site config here */
   siteMetadata: {
     title: "Supers Cafe",
-    description: "Supers Cafe – a life/professional portfolio site",
+    description: "Supers Cafe – A Life/Professional Portfolio Site",
     author: "@nh3r717",
     siteUrl: "https://supers.cafe",
   },
@@ -24,6 +24,26 @@ module.exports = {
       options: {
         name: `assets`,
         path: `${__dirname}/src/assets/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Comfortaa",
+              variants: ["300", "400", "500"],
+              fontDisplay: "swap",
+              strategy: "selfHosted",
+            },
+          ],
+        },
+        formatAgents: {
+          woff: `Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko`,
+          woff2: `Mozilla/5.0 (Windows NT 10.0; Win64; x64; ServiceUI 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393`,
+          ttf: `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.59.8 (KHTML, like Gecko) Version/5.1.9 Safari/534.59.8`,
+        },
       },
     },
   ],
