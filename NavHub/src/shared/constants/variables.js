e2981e8813be0e2fc32ecfa614b07d6bd0a5804c
tommy <500@superscafe.com>
2021-08-08 13:43:43 -0400

    update – colors / blog icons/buttons (opacity) & variables

--- NavHub/src/shared/constants/variables.js
+++ NavHub/src/shared/constants/variables.js
@@ -1,8 +1,15 @@
 // Brand Colors
 
 const colors = {
-  DarkGrey: "#5a5a5a",
-  LightGrey: "#5C5C5C",
+  BrandOrange: "rgba(245, 100, 70 1)",
+  BrandYellow: "rgba(250, 205, 90, 1)",
+  BrandViolet: "rgba(195, 75, 215, 1)",
+  BrandSeaFoam: "rgba(160, 220, 175, 1)",
+  BrandNavy: "rgba(65, 100, 245, 1)",
+  BrandGreyDark: "rgba(90, 90, 90, 1)",
+  IconOrange: "rgba(100, 80, 75, 1)",
+  BrandGreyMid: "rgba(92, 92, 92, 1)",
+  BrandGreyLight: "rgba(96, 96, 96, 1)",
 };
 
 export { colors };
