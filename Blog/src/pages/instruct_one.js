6c5c98acfd469f383c764190653ff42442c7b45e
tommy <500@superscafe.com>
2021-08-19 13:03:52 -0400

    update – add dir for specific web project (accomidates other web projects)

---
+++ Blog/src/pages/instruct_one.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "/src/shared/components/Layout.js"
+
+const index = () => {
+  return (
+    <Layout>
+      <section style={{ height: "200px", margin: "15% 0" }}>
+        <h1 style={{ color: "red" }}>Blog Page – How To</h1>
+        <h3 style={{ color: "red" }}>coming soon</h3>
+      </section>
+    </Layout>
+  )
+}
+
+export default index
